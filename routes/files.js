616cd88abf6cb14a96648737e7c39d27894d8555
Jayavenkatasatya Radhamadhavi Bhatta <JR00432239@techmahindra.com>
2018-05-14 23:40:58 +0530

    commit

--- routes/files.js
+++ routes/files.js
@@ -2,7 +2,8 @@ var express = require('express');
 var router = express.Router();
 var multer = require('multer');
 var fs = require('fs');
-
+var jsonfile = require('jsonfile');
+var arrayCompare = require("array-compare")
 var storage = multer.diskStorage({
     // destination
     destination: function (req, file, cb) {
@@ -31,46 +32,164 @@ router.post("/upload", upload.array("uploads[]", 12), function (req, res) {
 
 
 router.post('/loadfilescontent', function (req, res, next) {
-    var a = req.body;
+    var a = req.body.files;
     console.log(a);
-    var element = JSON.stringify(a);
-       console.log(element);
-         fs.readFile("./database/files.json", 'utf8', function(err, json) {
-             var array = JSON.parse(JSON.stringify(json));
-             console.log("old data:---" +array);
-             //var array = new Array();
-             var b = array.concat(element);
-             var c = JSON.parse(JSON.stringify(b));
-             console.log("new data:----" +c);
-             fs.appendFile("./database/files.json", c, function(err){
-               if (err) throw err;
-               console.log("saved");
-             });
-        console.log("The file was saved!");
-             });
+    files_data=files_data.concat(a);
+       var json_data_file={"total_files":files_data};
+        
+        jsonfile.writeFile("./database/files.json", json_data_file, function(err){
+                    if (err) throw err;
+                    console.log("saved");
+                  });
 res.end('{"msg": "success"}');
      });
 
-router.post('/comparejsons', function (req, res, next) {
-    //obj1={a:1,b:2,c:3,d:4};
-//obj2={a:2,b:5,c:3,d:4};
 
-var obj1 = "./database/files.json";
-var obj2 = "./database/file.json";
-function findKeysByValue(obj, v) {
-    var results = [];
-    for (var k in obj2) {
-        if (obj2.hasOwnProperty(k) && v == obj1[k]) {
-            results.push(k);
-        }
+
+router.post('/comparejson', function (req, res, next) {
+     
+var filename = './database/files.json';
+var filename1= './database/file.json';
+
+jsonfile.readFile(filename, function(err, jsonData) {
+    jsonfile.readFile(filename1, function(err, jsonData1) {
+  for (var i = 0; i < jsonData.length; ++i) {
+    for (var j = 0; j < jsonData1.length; ++j) {
+
+    console.log("Filename: "+jsonData[i].filename);
+    console.log("Filename: "+jsonData1[j].filename);
+
+    if( jsonData[i].filename===jsonData1[j].filename1){
+        console.log(jsonData[i].status==jsonData[j].status);
     }
-    return results;
+
+
+     }
+
+     
+       
+        
+      
+          
+           }
+
+          
+        
+
+        });
+
+    });
+})
+     /*router.post('/comparejson', function (req, res, next) {
+        var a;
+        fs.readFile('./database/files.json', 'utf8', function (err, data) {
+          if (err) throw err;
+          a = JSON.parse(data);
+         // console.log(a);
+          var b;
+       fs.readFile('./database/file.json', 'utf8', function (err, data) {
+          if (err) throw err;
+          b = JSON.parse(data);
+         // console.log(b);
+        
+        function remove_duplicates(a, b) {
+            for (var i = 0, len = a.length; i < len; i++) { 
+                for (var j = 0, len2 = b.length; j < len2; j++) { 
+                    if (a[i].total_files.filename === b[j].file.filename) {
+                        console.log(a[i].total_files.filename )
+                        //b.splice(j, 1);
+                        len2=b.length;
+                    }
+                }
+            }
+        
+            //console.log(a);
+            //console.log(b);
+        
+        }
+        
+        remove_duplicates(a,b);
+        
+         for (var i = 0, len = b.length; i < len; i++) {
+             console.log("filename:"+a[i].total_files.filename +" status:"+a[i].total_files.status);
+         }
+        
+        });
+       
+    });
+});*/
+       /* Object.keys(obj1).forEach(function(key){
+            console.log(key + ':' + obj1[key]);
+        })*/
+
+        /*if(obj1.hasOwnProperty('filename')){
+            console.log(filename);*/
+            //do something if the key exist
+     //  }
+    
+
+        /*fs.readFile("./database/file.json", 'utf8', function(err, json1) {
+            var obj2 = json1;
+            console.log("old data:---" +obj2);
+
+
+            JSON.stringify(obj1) == JSON.stringify(obj2) 
+            obj1['filename']=obj2['filename']*/
+    
+            //var obj1 = {'key':'value', 'key2':'value2'};
+            //for(var Key in obj1) {
+            //console.log("key:"+Key+", value:"+obj1[Key]);
+//}
+
+       
+
+
+    /*fs.readFile('./database/files.json', 'utf8', function (err,data) {
+        data = JSON.parse(data); 
+        data.forEach(function(elem) {
+            console.log(elem);
+        });// you missed that...
+       // console.log(data);*/
+
+      /* fs.readFile('./database/files.json').forEach(elem => {
+    
+        console.log(elem);
+    
+      });*/
+      
+      /*router.post('/comparejson', function (req, res, next) {
+        fs.readFile('./database/file.json', 'utf8', function (err,obj) {
+            obj = JSON.parse(obj); // you missed that...
+           // console.log(data1);
+
+           var tagMap = {};
+var i = null;
+for (i = 0; obj.length > i; i += 1) {
+    tagMap[obj[i].tagName] = obj[i];
 }
-console.log(findKeysByValue(obj2, obj1['status']));
+ 
+var hasTag = function(tagName) {
+    return tagMap[tagName];
+};
+
+           /*for (var key in obj) {
+            if (obj.hasOwnProperty(key)) {
+              console.log(key + ": " + obj[key]);
+            }
+          }​
+           
+           });
+    });*/
+    
 
 
-});
+//    if   file.jason.hasOwnproperty('filename1')then{ file.jason[status]  =files.jason[status]
 
 
 
+        
+
+
+     
 module.exports = router;
+     
