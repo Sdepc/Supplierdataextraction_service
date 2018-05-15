88095c247885c93f8fd811f2523984af05c726d1
Jayavenkatasatya Radhamadhavi Bhatta <JR00432239@techmahindra.com>
2018-05-15 16:32:10 +0530

    commit

--- routes/files.js
+++ routes/files.js
@@ -7,7 +7,7 @@ var arrayCompare = require("array-compare")
 var storage = multer.diskStorage({
     // destination
     destination: function (req, file, cb) {
-        cb(null, './uploads/')
+        cb(null, './inputs/')
     },
     filename: function (req, file, cb) {
         cb(null, file.originalname);
@@ -15,181 +15,58 @@ var storage = multer.diskStorage({
 });
 var upload = multer({ storage: storage });
 
-var files_data=[];
+var files_data = [];
 
 
 /* GET Files listing. */
 router.get('/getfilescontent', function (req, res, next) {
     var json = JSON.parse(fs.readFileSync('./database/files.json', 'utf8'));
     res.setHeader('Content-Type', 'application/json');
-    console.log(json);
+    //console.log(json);
     res.send(JSON.stringify(json));
-    });
+});
 router.post("/upload", upload.array("uploads[]", 12), function (req, res) {
-    console.log('files', req.files);
+    //console.log('files', req.files);
     res.send(req.files);
 });
 
 
-router.post('/loadfilescontent', function (req, res, next) {
-    var a = req.body.files;
-    console.log(a);
-    files_data=files_data.concat(a);
-       var json_data_file={"total_files":files_data};
-        
-        jsonfile.writeFile("./database/files.json", json_data_file, function(err){
-                    if (err) throw err;
-                    console.log("saved");
-                  });
+router.post('/savefilesjson', function (req, res, next) {
+    var input = req.body.files;
+    //console.log(input);
+    files_data = files_data.concat(input);
+    var json_data_file = { "total_files": files_data };
+
+    jsonfile.writeFile("./database/files.json", json_data_file, function (err) {
+        if (err) throw err;
+        //console.log("saved");
+    });
+    res.end('{"msg": "success"}');
+});
+
+
+
+
+
+
+
+
+
+router.post('/deletefilesjson',  function  (req,  res,  next) {
+var removeUser = req.query.filename;
+var data = fs.readFileSync('./database/files.json');
+var json = JSON.parse(data);
+console.log(json)
+var files = json.files;
+console.log(files);
+json.files = files.filter((file) => { return file.filename !== removeUser });
+console.log(json.files);
+fs.writeFileSync('./database/files.json', JSON.stringify(json, null));
 res.end('{"msg": "success"}');
-     });
+});
 
 
 
-router.post('/comparejson', function (req, res, next) {
-     
-var filename = './database/files.json';
-var filename1= './database/file.json';
-
-jsonfile.readFile(filename, function(err, jsonData) {
-    jsonfile.readFile(filename1, function(err, jsonData1) {
-  for (var i = 0; i < jsonData.length; ++i) {
-    for (var j = 0; j < jsonData1.length; ++j) {
-
-    console.log("Filename: "+jsonData[i].filename);
-    console.log("Filename: "+jsonData1[j].filename);
-
-    if( jsonData[i].filename===jsonData1[j].filename1){
-        console.log(jsonData[i].status==jsonData[j].status);
-    }
 
 
-     }
-
-     
-       
-        
-      
-          
-           }
-
-          
-        
-
-        });
-
-    });
-})
-     /*router.post('/comparejson', function (req, res, next) {
-        var a;
-        fs.readFile('./database/files.json', 'utf8', function (err, data) {
-          if (err) throw err;
-          a = JSON.parse(data);
-         // console.log(a);
-          var b;
-       fs.readFile('./database/file.json', 'utf8', function (err, data) {
-          if (err) throw err;
-          b = JSON.parse(data);
-         // console.log(b);
-        
-        function remove_duplicates(a, b) {
-            for (var i = 0, len = a.length; i < len; i++) { 
-                for (var j = 0, len2 = b.length; j < len2; j++) { 
-                    if (a[i].total_files.filename === b[j].file.filename) {
-                        console.log(a[i].total_files.filename )
-                        //b.splice(j, 1);
-                        len2=b.length;
-                    }
-                }
-            }
-        
-            //console.log(a);
-            //console.log(b);
-        
-        }
-        
-        remove_duplicates(a,b);
-        
-         for (var i = 0, len = b.length; i < len; i++) {
-             console.log("filename:"+a[i].total_files.filename +" status:"+a[i].total_files.status);
-         }
-        
-        });
-       
-    });
-});*/
-       /* Object.keys(obj1).forEach(function(key){
-            console.log(key + ':' + obj1[key]);
-        })*/
-
-        /*if(obj1.hasOwnProperty('filename')){
-            console.log(filename);*/
-            //do something if the key exist
-     //  }
-    
-
-        /*fs.readFile("./database/file.json", 'utf8', function(err, json1) {
-            var obj2 = json1;
-            console.log("old data:---" +obj2);
-
-
-            JSON.stringify(obj1) == JSON.stringify(obj2) 
-            obj1['filename']=obj2['filename']*/
-    
-            //var obj1 = {'key':'value', 'key2':'value2'};
-            //for(var Key in obj1) {
-            //console.log("key:"+Key+", value:"+obj1[Key]);
-//}
-
-       
-
-
-    /*fs.readFile('./database/files.json', 'utf8', function (err,data) {
-        data = JSON.parse(data); 
-        data.forEach(function(elem) {
-            console.log(elem);
-        });// you missed that...
-       // console.log(data);*/
-
-      /* fs.readFile('./database/files.json').forEach(elem => {
-    
-        console.log(elem);
-    
-      });*/
-      
-      /*router.post('/comparejson', function (req, res, next) {
-        fs.readFile('./database/file.json', 'utf8', function (err,obj) {
-            obj = JSON.parse(obj); // you missed that...
-           // console.log(data1);
-
-           var tagMap = {};
-var i = null;
-for (i = 0; obj.length > i; i += 1) {
-    tagMap[obj[i].tagName] = obj[i];
-}
- 
-var hasTag = function(tagName) {
-    return tagMap[tagName];
-};
-
-           /*for (var key in obj) {
-            if (obj.hasOwnProperty(key)) {
-              console.log(key + ": " + obj[key]);
-            }
-          }​
-           
-           });
-    });*/
-    
-
-
-//    if   file.jason.hasOwnproperty('filename1')then{ file.jason[status]  =files.jason[status]
-
-
-
-        
-
-
-     
 module.exports = router;
-     
--- routes/summary.js
+++ routes/summary.js
@@ -6,98 +6,94 @@ var fs = require('fs');
 
 
 /* GET home page.*/
-router.get('/contractnames', function(req, res, next) {
-
-  var testFolder = './output/';
-  var fs = require('fs');
-  
-  res.setHeader('Content-Type', 'application/json');
-  var obj =[];
-  fs.readdirSync(testFolder).forEach(file => {
-    
-    obj.push(path.basename(file,'.txt'));
-
-  });
-  
+router.get('/contractnames', function (req, res, next) {
+var input_path = './output/';
+var fs = require('fs');
+res.setHeader('Content-Type', 'application/json');
+  var obj = [];
+  fs.readdirSync(input_path).forEach(file => {
+  obj.push(path.basename(file, '.txt'));
+});
 res.write(JSON.stringify(obj, null, 3));
- });
-    
+});
 
 
- router.get('/contractcontent', function(req,res){
+
+router.get('/contractcontent', function (req, res) {
   var http = require('http');
-   var fs = require('fs');
-   //var path = require('path');
- var test1 = './output/';
- 
- 
- var a = req.query.filename;
- 
- //var a = '1525697142871Contract-A';
- console.log(a);
- fileExt = 'txt';
- b = a + '.' + fileExt;
- c = test1 + b;
- console.log(b);
- fs.readdir(test1, function(err, items){
-       console.log(items);
- fs.exists(c, function(exists){
-     console.log("file exists ? " , exists);
-     var file = fs.readFileSync(c, "utf8");
-     console.log(file);
-     res.writeHead(200,{"Content-Type" : "text/html"});
-     res.write('<html><p>'+file+'</p></html>');
- 
- 
- });
- });
- });
- 
+  var fs = require('fs');
+  //var path = require('path');
+  var input_path = './output/';
 
 
-   router.post('/purge', function (req, res, next) {
-    console.log('POST');
-    console.log(req.body);
-    var directory = 'output';
-    fs.readdir(directory, (err, files) => {
-      if (err) throw err;
-      for (const file of files) {
-        fs.unlink(path.join(directory, file), err => {
-          if (err) throw err;
-          res.send('successfully deleted');
-        });
-      }
+  var input = req.query.filename;
+
+  //var a = '1525697142871Contract-A';
+  //console.log(a);
+  var fileExtension = 'html';
+  var file_name = input + '.' + fileExtension;
+  var total_path = input_path + file_name;
+  //console.log(b);
+  fs.readdir(input_path, function (err, items) {
+    //console.log(items);
+    fs.exists(c, function (exists) {
+      //console.log("file exists ? ", exists);
+      var file = fs.readFileSync(c, "utf8");
+      //console.log(file);
+      //res.writeHead(200,{"Content-Type" : "text/html"});
+
+      res.send(file);
+
+
     });
   });
+});
 
 
 
- router.post('/oldfiles', function (req, res, next) {
-    console.log('POST');
-    console.log(req.body);
-//purging older files
-var fs = require('fs')
-var dirPath =  './output/';
-//var file = '/Contract-1.pdf'
-fs.readdir( dirPath, function( err, files ) {
-    if ( err ) return console.log( err );
-    files.forEach(function( file ) {
-        var filePath = dirPath + file;
-        fs.stat( filePath, function( err, stat ) {
-            if ( err ) return console.log( err );
-            var livesUntil = new Date();
-//setting hours i.e.(24*30) for purging 30 days older files
-            livesUntil.setHours(livesUntil.getHours() - 720 );
-            if ( stat.ctime < livesUntil ) {
-                fs.unlink( filePath, function( err ) {
-                    if ( err ) return console.log( err );
-                });
-              }
-				console.log("sucessfully deleted the older files");
-            });
-        });
+router.post('/purge', function (req, res, next) {
+  //console.log('POST');
+  //console.log(req.body);
+  var directory = 'output';
+  fs.readdir(directory, (err, files) => {
+    if (err) throw err;
+    for (const file of files) {
+      fs.unlink(path.join(directory, file), err => {
+        if (err) throw err;
+        res.send('successfully deleted');
+      });
+    }
+  });
+});
+
+
+
+router.post('/oldfiles', function (req, res, next) {
+  //console.log('POST');
+  //console.log(req.body);
+  //purging older files
+  var fs = require('fs')
+  var dirPath = './output/';
+  //var file = '/Contract-1.pdf'
+  fs.readdir(dirPath, function (err, files) {
+    if (err) return console.log(err);
+    files.forEach(function (file) {
+      var filePath = dirPath + file;
+      fs.stat(filePath, function (err, stat) {
+        if (err) return console.log(err);
+        var livesUntil = new Date();
+        //setting hours i.e.(24*30) for purging 30 days older files
+        livesUntil.setHours(livesUntil.getHours() - 720);
+        if (stat.ctime < livesUntil) {
+          fs.unlink(filePath, function (err) {
+            if (err) return console.log(err);
+          });
+        }
+        //console.log("sucessfully deleted the older files");
+      });
     });
   });
+});
 
 
 module.exports = router;
