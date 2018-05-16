b08b8d969938e38045a104df6e0ed6dbbeb11d64
Partha Saradhi Gajula -X (pgajula - TECH MAHINDRA LIM at Cisco) <pgajula@cisco.com>
2018-05-16 11:58:16 -0700

    updated one

--- routes/files.js
+++ routes/files.js
@@ -4,8 +4,8 @@ var multer = require('multer');
 var fs = require('fs');
 var jsonfile = require('jsonfile');
 var arrayCompare = require("array-compare")
+
 var storage = multer.diskStorage({
-    // destination
     destination: function (req, file, cb) {
         cb(null, './inputs/')
     },
@@ -13,56 +13,41 @@ var storage = multer.diskStorage({
         cb(null, file.originalname);
     }
 });
-var upload = multer({ storage: storage });
 
+var upload = multer({ storage: storage });
 var files_data = [];
 
+router.post("/upload", upload.array("uploads[]", 12), function (req, res) {
+    //console.log('files', req.files);
+    res.send(req.files);
+});
 
 /* GET Files listing. */
 router.get('/getfilescontent', function (req, res, next) {
     var json = JSON.parse(fs.readFileSync('./database/files.json', 'utf8'));
     res.setHeader('Content-Type', 'application/json');
     //console.log(json);
-    res.send(JSON.stringify(json));
+    res.send(json);
 });
-router.post("/upload", upload.array("uploads[]", 12), function (req, res) {
-    //console.log('files', req.files);
-    res.send(req.files);
-});
-
-
 
 router.put('/updatefilesjson', function (req, res, next) {
     var input = req.body.files;
-   
-    
-    var data = fs.readFileSync('./database/files.json','utf8');
+    var data = fs.readFileSync('./database/files.json', 'utf8');
     var json = JSON.parse(data);
-           
-  for(j in json){
-            for(y in json[j]){
-                if(json[j][y].filename===input[0].filename){
-                    json[j][y].status=input[0].status
-                    var total_data =  json[j][y].status
-                   //console.log(json)
-                    
-                }
-                
+    for (j in json) {
+        for (y in json[j]) {
+            if (json[j][y].filename === input[0].filename) {
+                json[j][y].status = input[0].status
+                var total_data = json[j][y].status
+                //console.log(json)
+
+            }
+
         }
     }
     fs.writeFileSync('./database/files.json', JSON.stringify(json), null);
     res.end('{"msg": "success"}');
-    
-
 });
-
-
-
-
-
-
-
-
 
 router.post('/savefilesjson', function (req, res, next) {
     var input = req.body.files;
@@ -76,15 +61,6 @@ router.post('/savefilesjson', function (req, res, next) {
     });
     res.end('{"msg": "success"}');
 });
-
-
-
-
-
-
-
-
-
 router.post('/deletefilesjson', function (req, res, next) {
     var removeUser = req.query.filename;
     var data = fs.readFileSync('./database/files.json');
@@ -97,9 +73,4 @@ router.post('/deletefilesjson', function (req, res, next) {
     fs.writeFileSync('./database/files.json', JSON.stringify(json, null));
     res.end('{"msg": "success"}');
 });
-
-
-
-
-
 module.exports = router;
