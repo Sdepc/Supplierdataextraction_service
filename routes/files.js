822cbf7f1fe25b3ea2232a53824cee50dda3db5f
Jayavenkatasatya Radhamadhavi Bhatta <JR00432239@techmahindra.com>
2018-05-16 14:09:43 +0530

    commit

--- routes/files.js
+++ routes/files.js
@@ -34,10 +34,32 @@ router.post("/upload", upload.array("uploads[]", 12), function (req, res) {
 
 router.put('/updatefilesjson', function (req, res, next) {
     var input = req.body.files;
-  
-
+   
+    
+    var data = fs.readFileSync('./database/files.json','utf8');
+    var json = JSON.parse(data);
+           
+  for(j in json){
+            for(y in json[j]){
+                if(json[j][y].filename===input[0].filename){
+                    json[j][y].status=input[0].status
+                    var total_data =  json[j][y].status
+                   //console.log(json)
+                    
+                }
+                
+        }
+    }
+    fs.writeFileSync('./database/files.json', JSON.stringify(json), null);
+    res.end('{"msg": "success"}');
+    
 
 });
+
+
+
+
+
 
 
 
@@ -63,17 +85,17 @@ router.post('/savefilesjson', function (req, res, next) {
 
 
 
-router.post('/deletefilesjson',  function  (req,  res,  next) {
-var removeUser = req.query.filename;
-var data = fs.readFileSync('./database/files.json');
-var json = JSON.parse(data);
-console.log(json)
-var files = json.files;
-console.log(files);
-json.files = files.filter((file) => { return file.filename !== removeUser });
-console.log(json.files);
-fs.writeFileSync('./database/files.json', JSON.stringify(json, null));
-res.end('{"msg": "success"}');
+router.post('/deletefilesjson', function (req, res, next) {
+    var removeUser = req.query.filename;
+    var data = fs.readFileSync('./database/files.json');
+    var json = JSON.parse(data);
+    console.log(json)
+    var files = json.files;
+    console.log(files);
+    json.files = files.filter((file) => { return file.filename !== removeUser });
+    console.log(json.files);
+    fs.writeFileSync('./database/files.json', JSON.stringify(json, null));
+    res.end('{"msg": "success"}');
 });
 
 
