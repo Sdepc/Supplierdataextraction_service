3b742fae30a6e6798d97448f3b769f25b2c20eb9
Shiva Kankala <SK00507400@techmahindra.com>
2018-05-18 13:50:10 +0530

    commit

--- scripts/P_htmltag.py
+++ scripts/P_htmltag.py
@@ -1,21 +1,19 @@
 def writehtmltag1(title):
         #insert html content
-        tag1 = "<!DOCTYPE html>"
-        tag1 += "<html>"
-        tag1 += "<head>"
         tag1 += "<h2>" + title.replace(".txt",".html") + "</h2>"
-        tag1 += "</head>"
-        tag1 += "<body style="'"font-size:21px"'"'>"
         tag1 += "<h3>Contract and Discount summary </h3>"
         tag1 += "<table border=1 border: 1px solid  bordercolor=black>"
         tag1 += "<div id="'"scrolltable"'">"
+        tag1 += "<ul class="list list--compressed">"
+        #tag1 += " <li>"
+        #tag1 +="  <div class="panel ">"
         return(tag1)
 
 def tablerow(xyz):
-        return("<tr bgcolor="'"white"'" ><td>" + xyz + "</td></tr>")
+        return("<li>""<div class="panel ">""<p class="paleWhite">" + xyz + "</p></div></li>")
 
 def tablerowy(xyz):
-        return("<tr bgcolor="'"yellow"'" ><td>" + xyz + "</td></tr>")
+        return("<li>""<div class="panel ">""<p class="paleYellow">"+ xyz + "</p></td></tr>")
 
 def tablerowb(xyz):
         return("<tr bgcolor="'"#f1e28a"'" ><td>" + xyz + "</td></tr>")
