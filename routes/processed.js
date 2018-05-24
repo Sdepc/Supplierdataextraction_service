7cfcbfa0d7fccb5ae3f14f6ada3d8ca6aa6da186
Shiva Kankala <SK00507400@techmahindra.com>
2018-05-24 11:29:36 +0530

    24-05-2108

--- routes/processed.js
+++ routes/processed.js
@@ -8,7 +8,9 @@ var PythonShell = require('python-shell');
 
 var fs = require('fs');
 var mv = require('mv');
-var fs1 = require('fs-extra')
+var fs1 = require('fs-extra')
+var path = require('path');
+
 
 app.use(express.static(__dirname + '/js'));
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -16,126 +18,163 @@ app.use(bodyParser.json());
 
 
 
-
-router.post('/moveFile', function (req, res, next) {
-    //  function moveFile(req,res,next){
+function moveFile(files) {
     var dirpath = './inputs/';
     var dir2 = './input_processing';
-    var files = req.body.files;
-    
+    //var files = ["Contract-1","Contract-2"]
     console.log(files);
-    
-    console.log(files);
-
     files.forEach(function (file) {
-        //console.log(files)
         console.log(file);
-
         var fileExtension = 'pdf';
         var file_name = file + '.' + fileExtension;
-
         console.log(file_name);
-        //console.log(file_name);
-
         var total_path = dirpath + file_name;
         console.log(total_path);
-        
-        var moveFile = (file_name, dir2) => {
+        var data = (file_name, dir2) => {
             //include the fs, path modules
-            var fs = require('fs');
-            var path = require('path');
 
             //gets file name and adds it to dir2
             var f = path.basename(file_name);
             var dest = path.resolve(dir2, f);
-
             fs.rename(file_name, dest, (err) => {
-                if (err) throw err;
-                else console.log('Successfully moved');
-
-
+                if (err) {
+                    throw err;
+                }
+                else {
+                    console.log('Successfully moved');
+                    updatefilesjson(UpdateFilename,"in process");
+                   //processData();
+                   
+                }
             });
         };
-        //move file1.htm from 'test/' to 'test/dir_1/'
-        moveFile(total_path, dir2);
-
+        data(total_path, dir2);
+        
     });
-    res.end("Successfully moved");
-});
+}
 
+function processData() {
+
+    var myPythonScriptPath = './scripts/P_Extract_Discounts.py';
+    console.log(myPythonScriptPath);
+    var pyshell = new PythonShell(myPythonScriptPath);
+    pyshell.on('message', function (message) {
+        console.log(message);
+        //res.setHeader('content-type', 'text/plain');
+        //res.send("to be processed");
+    });
+    pyshell.end(function (err) {
+
+        
+        if (err) {
+
+
+            //updatefilesjson(UpdateFilename,"process failed");
+            updatefilesjson();
+
+        }
+        else{
+        deletefilesjson();
+        }
+});
+}
+
+
+        
+
+
+//});
+//});
+
+
+
+function updatefilesjson(UpdateFilename, Status) {
+    var data = fs.readFileSync('./database/files.json', 'utf8');
+    var json = JSON.parse(data);
+    for (j in json) {
+        for (y in json[j]) {
+            if (json[j][y].filename === UpdateFilename) {
+                json[j][y].status = Status
+                var total_data = json[j][y].status
+
+            }
+        }
+    }
+    fs.writeFileSync('./database/files.json', JSON.stringify(json), null);
+    
+    //data(updateFilename,dir2);
+    console.log('updated');
+
+    fs1.move('./input_processing/', './inputs', function (err) {
+         if (err) return console.error(err)
+         console.log("success!")
+        }) 
+
+    //res.setHeader('content-type', 'text/plain');
+    //res.end("updated");
+
+}
+
+
+
+
+function deletefilesjson(removeFileName) {
+    var data = fs.readFileSync('./database/files.json');
+    var json = JSON.parse(data);
+    var files = json.files;
+    json.files = files.filter((file) => { return file.filename !== removeFileName });
+    fs.writeFileSync('./database/files.json', JSON.stringify(json, null));
+    console.log('deleted');
+    //return(json)
+    // res.setHeader('content-type', 'text/plain');
+    //res.send("processed");
+    res.write("processed");
+}
+
+    
+
+/*function backtrack(files) {
+    var dirpath = './input_processing/';
+    var dir2 = './inputs';
+    var files = ["Contract-1","Contract-2"]
+    console.log(files);
+    files.forEach(function (file) {
+        console.log(file);
+        var fileExtension = 'pdf';
+        var file_name = file + '.' + fileExtension;
+        console.log(file_name);
+        var total_path = dirpath + file_name;
+        console.log(total_path);
+        var data = (file_name, dir2) => {
+            //include the fs, path modules
+
+            //gets file name and adds it to dir2
+            var f = path.basename(file_name);
+            var dest = path.resolve(dir2, f);
+            fs.rename(file_name, dest, (err) => {
+                if (err) {
+                    throw err;
+                }
+                else {
+                    console.log('Successfully moved');
+                    //updatefilesjson(UpdateFilename,"in process");
+                    //processData();
+                   
+                }
+            });
+        };
+        data(total_path, dir2);
+        
+    });*/
+//}
 
 
 router.post('/pythonscripts', function (req, res, next) {
 
-   
+    moveFile(req.body.files);
+    processData();
 
-
-            res.setHeader('content-type', 'text/plain');
-            res.end("in process");
-            // Do something
-            var myPythonScriptPath = './scripts/P_Extract_Discounts.py';
-            console.log(myPythonScriptPath);
-            var pyshell = new PythonShell(myPythonScriptPath);
-            pyshell.on('message', function (message) {
-                console.log(message);
-                //res.setHeader('content-type', 'text/plain');
-                //res.send("to be processed");
-            });
-            pyshell.end(function (err) {
-                if (err) {
-                    updatefilesjson();
-                   
-            
-                console.log('finished');
-                deletefilesjson();
-               
-                }  
-                
-            });
-            
-              
-
-
-
-
-
-        });
-
-
-
-
-         function updatefilesjson(UpdateFilename, Status) {
-            var data = fs.readFileSync('./database/files.json', 'utf8');
-            var json = JSON.parse(data);
-            for (j in json) {
-                for (y in json[j]) {
-                    if (json[j][y].filename === UpdateFilename) {
-                        json[j][y].status = Status
-                        var total_data = json[j][y].status
-                    }
-                }
-            }
-            fs.writeFileSync('./database/files.json', JSON.stringify(json), null);
-            console.log('updated');
-            res.setHeader('content-type', 'text/plain');
-            res.end("updated");
-
-        }
-
-        
-      
-
-        function deletefilesjson(removeFileName) {
-            var data = fs.readFileSync('./database/files.json');
-            var json = JSON.parse(data);
-            var files = json.files;
-            json.files = files.filter((file) => { return file.filename !== removeFileName });
-            fs.writeFileSync('./database/files.json', JSON.stringify(json, null));
-            console.log('deleted');
-            res.setHeader('content-type', 'text/plain');
-            res.send("processed");
-        }
-
+});
 
 
 
